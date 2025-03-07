ca45ba12c3ff1e79badb5bdbc2e26771d4ffafed
VoDylan <vdylan24@gmail.com>
2025-03-07 12:44:00 +0800

    Updating stuff

--- src/routes/BookletReport.tsx
+++ src/routes/BookletReport.tsx
@@ -3,14 +3,14 @@ import "react-pdf/dist/Page/AnnotationLayer.css";
 import { pdfjs } from "react-pdf";
 import "../index.css"; // Assuming you have a CSS file for styles
 import PDFviewing from "../Components/PDFviewing.tsx";
-import beePDF from "../assets/BeetalesInfographic.pdf"
+import finalBooklet from "../assets/FinalBeetalesBooklet.pdf"
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 export default function BookletReport() {
     return (
         <div style={{ width: "100%", height: "auto", marginTop: "5rem" }}>
-            <PDFviewing src={beePDF}/>
+            <PDFviewing src={finalBooklet}/>
         </div>
     );
 }
